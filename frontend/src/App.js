774443d1af93fcb11b6e3f70f16ccee5afdb7e87
Tshady <kullnobita69@gmail.com>
2021-01-09 07:15:49 +0700

    Home screen, Detail Screen UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
-    <>
-      <h1>Welcome to TGHM</h1>
-    </>
+    <Router>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <h1>Wel come to TGHM</h1>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} exact />
+        </main>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
