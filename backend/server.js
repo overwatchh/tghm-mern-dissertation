4bd9c3347bdc4662b95757a5ce04239343749803
Tshady <kullnobita69@gmail.com>
2021-01-12 10:06:12 +0700

    set up data schema, api routes, seeding dummy data

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
-
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleWare.js";
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.send("Server is running...");
 });
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
