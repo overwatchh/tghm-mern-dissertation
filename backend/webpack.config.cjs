e5f3e34dfe63c156d61fdeb3b6c908e559c1c652
tuannd <kullnobita69@gmail.com>
2022-11-19 06:51:18 +0700

    dockerize the app

---
+++ backend/webpack.config.cjs
@@ -0,0 +1,39 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const environment = process.env.ENVIRONMENT;
+
+console.log("environment:::::", environment);
+
+let ENVIRONMENT_VARIABLES = {
+  "process.env.NODE_ENV": JSON.stringify("development"),
+  "process.env.PORT": JSON.stringify("5000"),
+  "process.env.MONGO_URI": JSON.stringify("mongodb://mongo-db:27017"),
+  "process.env.JWT_SECRET": JSON.stringify("abc123"),
+};
+
+if (environment === "test") {
+  ENVIRONMENT_VARIABLES = {
+    "process.env.NODE_ENV": JSON.stringify("development"),
+    "process.env.PORT": JSON.stringify("5000"),
+    "process.env.MONGO_URI": JSON.stringify("mongodb://mongo-db:27017"),
+    "process.env.JWT_SECRET": JSON.stringify("abc123"),
+  };
+} else if (environment === "production") {
+  ENVIRONMENT_VARIABLES = {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+    "process.env.PORT": JSON.stringify("5000"),
+    "process.env.MONGO_URI": JSON.stringify("mongodb://mongo-db:27017"),
+    "process.env.JWT_SECRET": JSON.stringify("abc123"),
+  };
+}
+
+module.exports = {
+  entry: "./server.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "api.bundle.js",
+  },
+  target: "node",
+  plugins: [new webpack.DefinePlugin(ENVIRONMENT_VARIABLES)],
+};
