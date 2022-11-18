b412731756657ed6b167b27b35af5d3b0416fcf6
tuannd <kullnobita69@gmail.com>
2022-11-19 06:51:41 +0700

    dockerize the app

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Stage1: UI Build
+FROM node:14-slim AS frontend-build
+WORKDIR /usr/src
+COPY frontend/ ./frontend/
+RUN cd frontend && npm install && npm run build
+
+# Stage2: API Build
+FROM node:14-slim AS backend-build
+WORKDIR /usr/src
+COPY backend/ ./backend/
+RUN cd backend && npm install && ENVIRONMENT=production npm run build
+RUN ls
+
+# Stage3: Packagign the app
+FROM node:14-slim
+WORKDIR /root/
+COPY --from=frontend-build /usr/src/frontend/build ./frontend/build
+COPY --from=backend-build /usr/src/backend/dist .
+RUN ls
+
+EXPOSE 80
+
+CMD ["node", "api.bundle.js"]
--- backend/webpack.config.cjs
+++ backend/webpack.config.cjs
@@ -16,14 +16,18 @@ if (environment === "test") {
   ENVIRONMENT_VARIABLES = {
     "process.env.NODE_ENV": JSON.stringify("development"),
     "process.env.PORT": JSON.stringify("5000"),
-    "process.env.MONGO_URI": JSON.stringify("mongodb://mongo-db:27017"),
+    "process.env.MONGO_URI": JSON.stringify(
+      "mongodb+srv://hang-ma-dich-xuyen:wmSuzy2o8bgZdz9Q@cluster0.e27lxes.mongodb.net/test"
+    ),
     "process.env.JWT_SECRET": JSON.stringify("abc123"),
   };
 } else if (environment === "production") {
   ENVIRONMENT_VARIABLES = {
     "process.env.NODE_ENV": JSON.stringify("production"),
     "process.env.PORT": JSON.stringify("5000"),
-    "process.env.MONGO_URI": JSON.stringify("mongodb://mongo-db:27017"),
+    "process.env.MONGO_URI": JSON.stringify(
+      "mongodb+srv://hang-ma-dich-xuyen:wmSuzy2o8bgZdz9Q@cluster0.e27lxes.mongodb.net/test"
+    ),
     "process.env.JWT_SECRET": JSON.stringify("abc123"),
   };
 }
